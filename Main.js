6fd511c5b2021e615b001fe959af80cba0b48ef6
Jamal Kainbayov <9023725@student.zadkine.nl>
2023-12-01 08:54:49 +0100

    Countdown

---
+++ Main.js
@@ -0,0 +1,28 @@
+
+var countDownDate = new Date("Jan 26, 2024 15:37:25").getTime();
+
+
+var x = setInterval(function() {
+
+  
+  var now = new Date().getTime();
+
+
+  var distance = countDownDate - now;
+
+ 
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
