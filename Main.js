b6461d6c9e464566b7368cb56512f2fa5ffbbb66
Jamal Kainbayov <9023725@student.zadkine.nl>
2023-12-08 13:15:12 +0100

    edits + quiz

--- Main.js
+++ Main.js
@@ -10,19 +10,19 @@ var x = setInterval(function() {
 
   var distance = countDownDate - now;
 
- 
+ //hier is een berekening te zien van elke stuk van de timer
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  
+  // 
   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
-  
+  //Als de timer onder de 0 is dan vertelt ie dat de open dag vandaag is 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("demo").innerHTML = "Vandaag is de open dag";
   }
 }, 1000);
