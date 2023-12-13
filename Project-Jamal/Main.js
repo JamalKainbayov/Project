6fd76a2b3a8e700e71682c9124d0d1441937f1d8
Jamal Kainbayov <9023725@student.zadkine.nl>
2023-12-13 12:18:11 +0100

    quiz af

--- Project-Jamal/Main.js
+++ Project-Jamal/Main.js
@@ -25,7 +25,7 @@ let questions =[
         question:"Ga je naar de open dag van het Techniek College Rotterdam?",
         answers: [
             {option:"Ja", answer:true},
-            {option:"Nee", answer:false}
+            {option:"Nee", answer:true}
         ]
 
 
@@ -51,5 +51,122 @@ submitBtn.addEventListener('click' , submit);
 function beginQuiz(){
     currentQuestion=0;
     questionText.innerHTML = questions[currentQuestion].question;
-    trueBtn.innerHTML
+    trueBtn.innerHTML = questions[currentQuestion].answers[0].option;
+    trueBtn.onclick = () => {
+        let ano=0;
+        if (questions [currentQuestion].answers[ano].answer){
+            if (score<3){
+                score++;
+            }
+        }
+        userScore.innerHTML= score;
+        if(currentQuestion<2){
+            next();
+        }
+    }
+    falseBtn.innerHTML = questions[currentQuestion].answers[1].option;
+    falseBtn.onclick = () => {
+        let ano=1;
+        if(questions[currentQuestion].answers[ano].answer){
+            if(score<3){
+                score++;
+            }        
+    }
+    prevBtn.classList.add('hide');
+    }
+}
+beginQuiz();
+
+function restart(){
+    currentQuestion = 0;
+    prevBtn.classList.remove('hide');
+    nextBtn.classList.remove('hide');
+    submitBtn.classList.remove('hide');
+    trueBtn.classList.remove('hide');
+    falseBtn.classList.remove('hide');
+    score = 0;
+    userScore.innerHTML = score;
+    beginQuiz();
+}
+
+function next(){
+    currentQuestion++;
+    if(currentQuestion>2){
+        nextBtn.classList.add('hide');
+        prevBtn.classList.remove('hide');
+    }
+questionText.innerHTML = questions[currentQuestion].question;
+trueBtn.innerHTML = questions[currentQuestion].answers[0].option;
+trueBtn.onclick = () => {
+    let ano=0;
+    if(questions[currentQuestion].answers[ano].answer){
+        if(score<3){
+            score++;
+        }
+    }
+    userScore.innerHTML = score;
+    if(currentQuestion<2){
+        next();
+    }
+}
+falseBtn.innerHTML = questions[currentQuestion].answers[1].option;
+falseBtn.onclick = () => {
+    let ano=1;
+    if(questions[currentQuestion].answers[ano].answer){
+        if(score<3){
+            score++;
+        }
+    }
+    userScore.innerHTML = score;
+    if(currentQuestion<2){
+        next();
+    }
+ }
+ prevBtn.classList.remove('hide');
+}
+
+function prev(){
+    currentQuestion--;
+    if(currentQuestion<=0){
+        prevBtn.classList.add('hide');
+        nextBtn.classList.remove('hide');
+    }
+    questionText.innerHTML = questions[currentQuestion].question;
+    trueBtn.innerHTML = questions[currentQuestion].answers[0].option;
+    trueBtn.onclick = () => {
+        let ano=0;
+        if(questions[currentQuestion].answers[ano].answer){
+            if(score<3){
+                score++;
+            }
+        }
+        userScore.innerHTML = score;
+        if(currentQuestion<2){
+            next();
+        }
+    }
+    falseBtn.innerHTML = questions[currentQuestion].answers[1].option;
+    falseBtn.onclick = () => {
+        let ano=1;
+        if(questions[currentQuestion].answers[ano].answer){
+        if(score<3){
+            score++;
+        }
+    }
+    userScore.innerHTML = score;
+    if(currentQuestion<2){
+        next();
+    }
+}
+nextBtn.classList.remove('hide');
+}
+
+function submit(){
+    prevBtn.classList.add('hide');
+    nextBtn.classList.add('hide');
+    submitBtn.classList.add('hide');
+    trueBtn.classList.add('hide');
+    falseBtn.classList.add('hide');
+    questionText.innerHTML = "Dank je wel voor het maken van de quiz!!";
+    document.getElementById('container').appendChild(element);  
 }
